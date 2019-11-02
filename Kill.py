b03435143e08ad9d0f76109a2a42b7792899657c
SanoHD <55959571+SanoHD@users.noreply.github.com>
2019-11-02 14:38:36 +0100

    Add files via upload

--- Kill.py
+++ Kill.py
@@ -1,9 +1,13 @@
 import sys, os
-
 try: file = open(sys.argv[1])
 except: print("[KILL] Invalid file!"); sys.exit()
 fr = file.read().strip()
 file.close()
 if fr == "!": os.remove(sys.argv[1])
 else: print("[KILL] Invalid command!"); sys.exit()
-   
+"""To make the interpreter bigger, i decided to create a story...
+A long time ago, there was a man. Big, attractive, stupid.
+Because of his stupidity, he build hisself a robot.
+This robot killed the man, because robots are evil.
+Like this "Programming Language".
+Dont do drugs, kids."""
